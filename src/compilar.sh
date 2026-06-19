7f4397d96fac410da1d670af8cf4b80949e68f67
atomicpenguinz <heitorlcaetano@gmail.com>
2026-06-19 19:18:53 -0300

    melhorando comando de compilação

--- src/compilar.sh
+++ src/compilar.sh
@@ -2,4 +2,4 @@
 
 # comando padrão para compilar o código.
 # Mais instruções no relatório
-gcc ls_adj.c hash.c arquivo.c main.c -O2 -o grafo
+gcc ./vendor/fort.c ls_adj.c hash.c arquivo.c main.c -O2 -o grafo $1
