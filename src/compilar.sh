0d9ac8a33a0b93d05cc9e472262148567dfe6a0d
atomicpenguinz <heitorlcaetano@gmail.com>
2026-06-19 21:53:33 -0300

    novo parâmetro

--- src/compilar.sh
+++ src/compilar.sh
@@ -2,4 +2,4 @@
 
 # comando padrão para compilar o código.
 # Mais instruções no relatório
-gcc ./vendor/fort.c ls_adj.c hash.c arquivo.c main.c -O2 -o grafo $1
+gcc ./vendor/fort.c ls_adj.c hash.c arquivo.c main.c dijkstra.c -O2 -o grafo $1
